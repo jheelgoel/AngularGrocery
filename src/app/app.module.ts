a72b9962fa51ca8736fdb0ab41e3569cdeea667d
Jheel Goel <mailjheel@gmail.com>
2022-07-14 12:17:58 -0700

    project setup and component building

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//import {HttpModule} from '@angular/http'
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { AppGroceryComponent } from './app-grocery/app-grocery.component';
+import { GroceryComponent } from './app.grocery';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppGroceryComponent,
+    GroceryComponent
   ],
+  
+  bootstrap: [AppComponent , GroceryComponent],
+  
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
+/*@NgModule({
+  declarations: [
+    GroceryComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [GroceryComponent]
+})*/
